32bc281271b65ca5f9aaf1744affcd87074b0614
yugandhar-pc <yugandhardcs21@gmail.com>
2018-05-15 17:56:15 +0530

    Observable Layout File is modified

--- app/src/main/java/com/example/yugandhar_pc/databindingexample/ObservablesExample.java
+++ app/src/main/java/com/example/yugandhar_pc/databindingexample/ObservablesExample.java
@@ -1,13 +1,18 @@
 package com.example.yugandhar_pc.databindingexample;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.yugandhar_pc.databindingexample.databinding.ActivityObservablesExampleBinding;
 
 public class ObservablesExample extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_observables_example);
+//        setContentView(R.layout.activity_observables_example);
+        ActivityObservablesExampleBinding binding = DataBindingUtil.setContentView(this, R.layout
+                .activity_observables_example);
     }
 }
